a18244534c1c0f7ee31bac691bf26bca37af4614
negri.gioele <negri.gioele@TAVE01.OSDBSESTO.local>
2017-04-08 11:33:41 +0200

    Ho aggiornato i files

--- Pub/src/model/Menu.java
+++ Pub/src/model/Menu.java
@@ -1,7 +1,11 @@
 package model;
 
 import java.util.HashMap;
-
+    /**
+     * menu del ristorante
+     * contiene una hashmap contente i prodotti con chiave: nome e valore: prezzo
+     * @author negri.gioele
+     */
 public class Menu {
     private HashMap<String,Integer> prodotti = new HashMap<String,Integer>();
     
@@ -19,5 +23,20 @@ public class Menu {
      */
     public void setNuovoProdotto(String a, int b){
             
+    }
+    /**
+     *  Rimuove il prodotto specificato
+     * @param a nome del prodotto
+     */
+    public void removeProdotto(String a){
+        
+    }
+    /**
+     * Restituisce il prezzo di un prodotto dato in ingresso il nome
+     * @param a nome prodotto
+     * @return prezzo
+     */
+    public int getPrezzo(String a){
+        return prodotti.get(a);
     }
 }
