5b7810276388cda4b75ae7cae1393e12ebbee7fa
negri.gioele <negri.gioele@TAVE01.OSDBSESTO.local>
2017-04-08 11:44:05 +0200

    Ho aggiornato i files

--- Pub/src/model/Ordinazione.java
+++ Pub/src/model/Ordinazione.java
@@ -1,5 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+
+/**
+ * Classe per le ordinazioni dei clienti
+ * Gestisce bevande, alimenti e numero del tavolo
+ * @author negri.gioele
+ */
 public class Ordinazione {
-    
+    private int NumeroTavolo;
+    private ArrayList<String> bevande = new ArrayList<String>();
+    private ArrayList<String> alimenti = new ArrayList<String>();
+    /**
+     * setta il numero del tavolo
+     * @param a numero del tavolo
+     */
+    public Ordinazione(int a){
+        NumeroTavolo = a;
+    }
+    /**
+     * restituisce il numero del tavolo a cui corrisponde l'ordinazione
+     * @return numero del tavolo
+     */
+    public int getNumeroTavolo(){
+        return NumeroTavolo;
+    }
 }
